d10e79840d1611a39dbeaa90f790ff7dfec9c84f
Benoît Sauvère <benoit.sauvere@gmail.com>
2016-03-11 10:45:51 +0100

    Modification de la Data Source pour le connecter au serveur RDS bac à sable

---
+++ TP-Couches-Donnees/Data/Migrations/Configuration.cs
@@ -0,0 +1,28 @@
+namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Data.Model>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Data.Model context)
+        {
+
+            context.Recipes.AddOrUpdate(
+              p => p.Id,
+              new RecipeDB { Id = Guid.Parse("EE4C3AE1-24EC-4CED-B1D7-D37EC5410719"), Name = "Recette1" },
+              new RecipeDB { Id = Guid.Parse("EE4C3AE1-24EC-4CED-B1D7-D37EC5410711"), Name = "Recette2" },
+              new RecipeDB { Id = Guid.Parse("EE4C3AE1-24EC-4CED-B1D7-D37EC5410712"), Name = "Recette3" },
+              new RecipeDB { Id = Guid.Parse("00000000-0000-0000-0000-000000000001"), Name = "MONSIEUR LE MAIRE DE TARABEL MIGEON" }
+            );
+
+        }
+    }
+}
