4c910222f2e8257986914528e5b2085ae6ca460f
Benoît Sauvère <benoit.sauvere@gmail.com>
2016-03-11 09:28:40 +0100

    Ajout des fichiers du prof, pour "repartir" d'un projet IHM corrrect

---
+++ CORRECTION/Formation1/Client/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Client
+    {
+        public event EventHandler<MessageEventArgs> SendMessageDone;
+        public string Nom { get; set; }
+
+        public void SendMessage(string message)
+        {
+            if (Nom == "Client #23")
+            {
+                throw new ArgumentException("Message incorrect", nameof(message));
+            }
+
+            if (SendMessageDone != null)
+            {
+                SendMessageDone(this, new MessageEventArgs { Message = message });
+            }
+        }
+    }
+
+    public class MessageEventArgs: EventArgs
+    {
+        public string Message { get; set; }
+    }
+
+
+}
---
+++ CORRECTION/Formation1/Console/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    class Program
+    {
+        private static Server.Server server;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                server = new Server.Server();
+                var listeClients = new List<Client.Client>();
+
+                for (int i = 0; i < 50; i++)
+                {
+                    var client = new Client.Client {Nom = "Client #" + i};
+                    client.SendMessageDone += OnSendMessageDone;
+                    listeClients.Add(client);
+                }
+
+                var listeReduite = listeClients.Skip(3).Take(40).OrderBy(client => client.Nom);
+
+                foreach (var client in listeReduite)
+                {
+                    client.SendMessage($"Message de {client.Nom} à {DateTime.Now}");
+                }
+            }
+            catch (ArgumentException exception)
+            {
+                System.Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine("ERREUR : " + exception.StackTrace);
+            }
+
+        }
+
+        private static void OnSendMessageDone(object sender, Client.MessageEventArgs eventArgs)
+        {
+            server.WriteMessage(eventArgs.Message);
+        }
+    }
+}
