c2411e7378e7e645bd3f1547218e7d88ad7086cb
Benoît Sauvère <benoit.sauvere@gmail.com>
2016-04-01 10:44:57 +0200

    WCF - Exercice 1 - Flow de service OK

---
+++ WCF2/WCF2-Meteo/Service/MeteoService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class MeteoService : ServiceContract.MeteoServiceContract
+    {
+        public int GetTemperature()
+        {
+            return new Random().Next(0, 50);
+        }
+    }
+}
---
+++ WCF2/WCF2-Meteo/ServiceContract/MeteoServiceContract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContract
+{
+    [System.ServiceModel.ServiceContract]
+    public interface MeteoServiceContract
+    {
+        [System.ServiceModel.OperationContract]
+        Int32 GetTemperature();
+    }
+}
